f59b997a8c687bb19e67d3e38262a11832bc50d0
v-nyadav <v-nyadav@D-441007775>
2016-08-31 13:04:43 +0530

    changes for test.

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
+
 var nconf = require('nconf');
 var express = require('express');
 var http = require('http');
+
 var path = require('path');
 var app = express();
 //var routes = require('./routes');
-var user=require('./routes/user');
+var user = require('./routes/user');
+
 var bodyParser = require('body-parser')
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -20,6 +24,7 @@ app.all('*', function(req, res, next) {
  });
 
 // all environments
+
 app.set('port', process.env.PORT || 3113);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,11 +35,13 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', routes);
 
 // tell nconf which config file to use
 nconf.env();
+
 nconf.file({ file: 'config.json' });
 
 var url = nconf.get("HOST");
